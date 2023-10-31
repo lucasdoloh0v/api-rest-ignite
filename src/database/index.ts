02c469a64c30ce935a89402c21e2e6c040fc5561
Lucas Alves <lucasdolohov@gmail.com>
2023-10-31 20:11:02 -0300

    feat: add .env

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,10 @@
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from '../env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './src/database/app.db',
+    filename: env.DATABASE_URL,
   },
   migrations: {
     extension: 'ts',
