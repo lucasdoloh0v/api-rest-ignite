d636da0e64235af3cd4e38bd2227ea45c1a8f264
Lucas Alves <lucasdolohov@gmail.com>
2023-11-01 05:55:07 -0300

    test: e2e tests

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,4 @@
-import fastify from 'fastify'
-import fastifyCookie from '@fastify/cookie'
-
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-
-const app = fastify()
-
-app.register(fastifyCookie)
-app.register(transactionsRoutes, { prefix: 'transactions' })
 
 app.listen({ port: env.PORT }).then(() => console.log('HTTP server running'))
